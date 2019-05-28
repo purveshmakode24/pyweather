33d079908c93559cc43c414c9abbc53a2c8d9bd2
Purvesh Makode <makodepurvesh@gmail.com>
2019-05-28 23:45:08 +0530

    Debug set to False

--- pyweather/settings.py
+++ pyweather/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c#e&185xyr^tqiubi7k%m)e^_62-lye6n=vd)&p4*que%nv@ii'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
